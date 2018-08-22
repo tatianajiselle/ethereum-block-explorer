22ebde3aa3ffac20242a9f2bb4f6d57a34ad513c
tatianajiselle <tjexoxo@gmail.com>
2018-08-21 23:05:25 -0700

    added ability to query range of blocks and parse user input

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var Web3 = require('web3');
 var prompt = require('prompt');
+var currentBlockNumber = 0; 
 
 prompt.start();
 
@@ -18,36 +19,48 @@ prompt.get(['number'], function (err, result) {
     const web3 = new Web3(web3Provider);
 
     if (result.number === '1'){
-        singleNumberInput(1,web3);
+        singleNumberInput(web3);
      } else if (result.number === 2){
 
      } else {
         console.error(error);
-        break;
     }
 });
 
 function singleNumberInput(web3) {
-    getCurrentBlockNumber();
+    
+    console.log("Please input a single number representing how far back from present block to search. Ex.) 10");
+    console.log("Max number you can input is equal to the current block number:");
+    getCurrentBlockNumber(web3);
+    
+    prompt.get(['number'], function (err, result) {
+        if (result.number <= currentBlockNumber){
+            var start = (currentBlockNumber - result.number);
+            doubleNumberInput(start, result.number, web3);
+         } else {
+             console.error(error + "Please pick a number that is within the current block number range.")
+         }
+
+    });
+
+   
 }
 
-function doubleNumberInput(x,y) {
-
-    return x,y;
+function doubleNumberInput(start, end, web3) {
+    while (start <= end){
+        console.log("Fetching block number:" + start)
+        fetchSingleBlock(start,web3);
+        console.log();
+        start++;
+    }
+    //return x,y;
 }
 
 
-// fetch block #
-function fetchSingleBlock(web3) {
-    web3.eth.getBlock(1, function(error, result){
-        if(!error)
-        return result;
-            //console.log(JSON.stringify(result));
-        else
-            console.error(error);
-            break;
-    })
-}
+
+
+
+
 
 //                  //
 // HELPER FUNCTIONS // 
@@ -60,16 +73,24 @@ function welcomeInstructions(){
     console.log("2. Input two numbers defining the start and end block numbers to search an inclusive range. Ex.) 8 10")
 }
 
+// fetch block #
+function fetchSingleBlock(number,web3) {
+    web3.eth.getBlock(number,function(error, result){
+        if(!error)
+            console.log(JSON.stringify(result));
+        else
+            console.error(error);
+    })
+}
+
 function getCurrentBlockNumber(web3){
     web3.eth.getBlockNumber(function(error, result){
         if(!error){
-            return result;
-            // console.log(JSON.stringify(result));
+            console.log(JSON.stringify(result));
+            currentBlockNumber = JSON.stringify(result);
         } else {
             console.error(error);
-            break;
-        }
-            
+        }   
     })
 }
 
